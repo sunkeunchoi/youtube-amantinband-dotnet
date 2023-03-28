40ccb6b55f5f1ffc820e041a2702d3a2686c22cf
sunkeunchoi <sunkeun.choi@gmail.com>
2023-03-28 17:47:44 +0900

    Inject AuthenticationSerive to Controller

--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,6 +1,10 @@
+using BuberDinner.Application.Services.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
     builder.Services.AddControllers();
+
 }
 
 var app = builder.Build();
--- BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,7 @@
 namespace BuberDinner.Application.Services.Authentication;
 public class AuthenticationService : IAuthenticationService
 {
-    public AuthenticationResult Login(string firstName, string lastName, string email, string password)
+    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
         return new AuthenticationResult(
             Guid.NewGuid(),
@@ -12,7 +12,7 @@ public class AuthenticationService : IAuthenticationService
         );
     }
 
-    public AuthenticationResult Register(string email, string password)
+    public AuthenticationResult Login(string email, string password)
     {
         return new AuthenticationResult(
             Guid.NewGuid(),
