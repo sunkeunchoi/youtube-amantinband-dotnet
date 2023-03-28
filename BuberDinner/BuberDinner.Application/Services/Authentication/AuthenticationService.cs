399a833e6ad469276d72331ad28d6343cf42200b
sunkeunchoi <sunkeun.choi@gmail.com>
2023-03-28 17:36:51 +0900

    Authentication Service

---
+++ BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,25 @@
+namespace BuberDinner.Application.Services.Authentication;
+public class AuthenticationService : IAuthenticationService
+{
+    public AuthenticationResult Login(string firstName, string lastName, string email, string password)
+    {
+        return new AuthenticationResult(
+            Guid.NewGuid(),
+            firstName,
+            lastName,
+            email,
+            "token"
+        );
+    }
+
+    public AuthenticationResult Register(string email, string password)
+    {
+        return new AuthenticationResult(
+            Guid.NewGuid(),
+            "firstName",
+            "lastName",
+            email,
+            "token"
+        );
+    }
+}
---
+++ BuberDinner/BuberDinner.Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,6 @@
+namespace BuberDinner.Application.Services.Authentication;
+public interface IAuthenticationService
+{
+    AuthenticationResult Login(string firstName, string lastName, string email, string password);
+    AuthenticationResult Register(string email, string password);
+}
