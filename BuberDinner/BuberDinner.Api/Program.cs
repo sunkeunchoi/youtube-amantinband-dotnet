136edef927d4275c9ba9c03cfe5def92b7a6d250
sunkeunchoi <sunkeun.choi@gmail.com>
2023-03-28 19:06:23 +0900

    Using Dependency Injection Infrastructure

--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,9 +1,8 @@
 using BuberDinner.Application;
-using BuberDinner.Application.Services.Authentication;
-
+using BuberDinner.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 {
-    builder.Services.AddApplication();
+    builder.Services.AddApplication().AddInfrastructure();
     builder.Services.AddControllers();
 }
 
