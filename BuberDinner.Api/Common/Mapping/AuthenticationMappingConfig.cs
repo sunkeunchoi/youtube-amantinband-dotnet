95b0aea7329f94c7d1af848eea22ef2f821f9f65
sunkeunchoi <sunkeun.choi@gmail.com>
2023-03-31 16:10:02 +0900

    Fix async warning and add all mappings

--- BuberDinner.Api/Common/Mapping/AuthenticationMappingConfig.cs
+++ BuberDinner.Api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -1,4 +1,6 @@
+using BuberDinner.Application.Authentication.Commands.Register;
 using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Authentication.Queries.Login;
 using BuberDinner.Contracts.Authentication;
 
 using Mapster;
@@ -8,8 +10,9 @@ public class AuthenticationMappingConfig : IRegister
 {
   public void Register(TypeAdapterConfig config)
   {
+    config.NewConfig<RegisterRequest, RegisterCommand>();
+    config.NewConfig<LoginRequest, LoginQuery>();
     config.NewConfig<AuthenticationResult, AuthenticationResponse>()
-      .Map(dest => dest.Token, src => src.Token)
       .Map(dest => dest, src => src.User);
   }
 }
--- BuberDinner.Application/Authentication/Queries/Login/LoginQueryHandler.cs
+++ BuberDinner.Application/Authentication/Queries/Login/LoginQueryHandler.cs
@@ -25,6 +25,7 @@ public class LoginQueryHandler : IRequestHandler<LoginQuery, ErrorOr<Authenticat
 
   public async Task<ErrorOr<AuthenticationResult>> Handle(LoginQuery query, CancellationToken cancellationToken)
   {
+    await Task.CompletedTask;
     // 1. Check if user exists
     if (_userRepository.GetUserByEmail(query.Email) is not User user)
     {
